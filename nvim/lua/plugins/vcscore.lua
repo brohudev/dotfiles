63249da60805b36f057ecf443ba1bf32a817524d
Brohudev <hitarth.thanki9006@gmail.com>
2025-01-05 21:14:03 -0600

    autosession worky lfggg
    all it took was reading and copy pasting from the repo instead of chat
    of somewhere else xD

--- nvim/lua/plugins/vcscore.lua
+++ nvim/lua/plugins/vcscore.lua
@@ -1,19 +1,40 @@
 return {
   {
     'rmagatti/auto-session',
-    config = function()
-      local auto_session = require 'auto-session'
-
-      auto_session.setup {
-        auto_restore_enabled = false,
-        auto_session_suppress_dirs = { '~/', '~/Dev/', '~/Downloads', '~/Documents', '~/Desktop/' },
-      }
-
-      local keymap = vim.keymap
-
-      keymap.set('n', '<leader>wr', '<cmd>SessionRestore<CR>', { desc = 'Restore session for cwd' }) -- restore last workspace session for current directory
-      keymap.set('n', '<leader>ws', '<cmd>SessionSave<CR>', { desc = 'Save session for auto session root dir' }) -- save workspace session for current working directory
-    end,
+    lazy = false,
+    keys = {
+      -- Will use Telescope if installed or a vim.ui.select picker otherwise
+      { '<leader>xf', '<cmd>SessionSearch<CR>', desc = 'Session search' },
+      { '<leader>xs', '<cmd>SessionSave<CR>', desc = 'Save session' },
+      { '<leader>xa', '<cmd>SessionToggleAutoSave<CR>', desc = 'Toggle autosave' },
+    },
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      -- ⚠️ This will only work if Telescope.nvim is installed
+      -- The following are already the default values, no need to provide them if these are already the settings you want.
+      session_lens = {
+        -- If load_on_setup is false, make sure you use `:SessionSearch` to open the picker as it will initialize everything first
+        load_on_setup = true,
+        previewer = false,
+        mappings = {
+          -- Mode can be a string or a table, e.g. {"i", "n"} for both insert and normal mode
+          delete_session = { { 'i', 'n' }, '<C-D>' },
+          alternate_session = { { 'i', 'n' }, '<C-S>' },
+          copy_session = { { 'i', 'n' }, '<C-Y>' },
+        },
+        -- Can also set some Telescope picker options
+        -- For all options, see: https://github.com/nvim-telescope/telescope.nvim/blob/master/doc/telescope.txt#L112
+        theme_conf = {
+          border = true,
+          -- layout_config = {
+          --   width = 0.8, -- Can set width and height as percent of window
+          --   height = 0.5,
+          -- },
+        },
+      },
+    },
   },
   {
     'kdheepak/lazygit.nvim',
